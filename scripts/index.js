9cdb0375eea7dc2389a4a70293be01a806cb3176
Adrianna Wagner <wagadr@outlook.com>
2025-11-21 23:00:12 -0500

    Final Stage Project Spots initial commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,54 @@
 const editProfileButton = document.querySelector(".profile__edit-button");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseButton = editProfileModal.querySelector(".modal__close-button");
-
-editProfileButton.addEventListener("click", function () {
-  editProfileModal.classList.add("modal_is-opened")
-});
-
-editProfileCloseButton.addEventListener("click", function () {
-  editProfileModal.classList.remove(".modal_is-opened")
-});
-
+const profileFormElement = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector("#profile-name-input");
+const editProfileDescriptionInput = editProfileModal.querySelector("#profile-description-input");
 
 const newPostButton = document.querySelector(".profile__post-button");
 const newPostModal = document.querySelector("#new-post-modal");
-const newPostCloseButton = editProfileModal.querySelector(".modal__close-button");
+const newPostCloseButton = newPostModal.querySelector(".modal__close-button");
+const addCardFormElement = newPostModal.querySelector(".modal__form");
+const newPostNameInput = newPostModal.querySelector("#card-caption-input");
+const newPostLinkInput = newPostModal.querySelector("#card-image-input");
+
+
+const profileNameElement = document.querySelector("profile__name");
+const profileDescriptionElement = document.querySelector("profile__description";)
+
+
 
 newPostButton.addEventListener("click", function () {
-  newPostModal.classList.add("modal_is-opened")
+  newPostModal.classList.add("modal_is-opened");
 });
 
 newPostCloseButton.addEventListener("click", function () {
-  newPostModal.classList.remove(".modal_is-opened")
+  newPostModal.classList.remove(".modal_is-opened");
 });
 
+editProfileButton.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameElement.textContent;
+  editProfileDescriptionInput.value = profileDescriptionElement.textContent;
+  editProfileModal.classList.add("modal_is-opened");
+});
+
+editProfileCloseButton.addEventListener("click", function () {
+  editProfileModal.classList.remove(".modal_is-opened");
+});
+
+function handleEditProfileSubmit {
+  evt.preventDefault();
+  profileNameElement.textContent = editProfileNameInput.value;
+  profileDescriptionElement.textContent = editProfileDescriptionInput.value;
+  editProfileModal.classList.remove(".modal_is-opened");
+};
+
+profileFormElement.addEventListener("submit", handleEditProfileSubmit);
+
+function handleAddCardSubmit(evt) {
+  evt.preventDefault();
+  console.log("nameInput.value", "linkInput.value");
+  newPostModal.classList.remove(".modal_is-opened");
+}
+
+  addCardFormElement.addEventListener("submit", handleAddCardSubmit);
